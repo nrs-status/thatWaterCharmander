eece57ea3ae6e40b0cebc89cd7ae4c1abf235f9f
nrs-status <sebaarsim@gmail.com>
2025-05-02 05:09:02 +0000

    adding nickel to sieyes userpkgs

--- zeus_olympia/sieyesUserPackages/default.nix
+++ zeus_olympia/sieyesUserPackages/default.nix
@@ -1,6 +1,6 @@
 { lclInputs, pkgs, ... }: {
   shared.users.users.sieyes = {
-    packages = (with pkgs; [ firefox kitty wofi btop mods ])
+    packages = (with pkgs; [ nickel firefox kitty wofi btop mods ])
       ++ (with lclInputs.frontArmToPlanePackages;
         [ montezuma_circles_scroll.nixvim.base ]);
   };
